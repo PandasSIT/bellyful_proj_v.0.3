8eef61820be950df7e3d1bee49cadff2aa781faa
jerryguo255 <jerryguo255@outlook.com>
2019-09-02 22:38:58 +1200

    Add project files.

---
+++ bellyful_proj_v.0.3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bellyful_proj_v._0._3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
